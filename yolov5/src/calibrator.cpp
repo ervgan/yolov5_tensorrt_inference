196da60c70c3a1bdf906ab059885c4806c986f9c
Ervin <ervin.gan@voliro.com>
2023-02-06 16:33:44 +0100

    deleted images shorcut in the yolov5 folder

--- yolov5/src/calibrator.cpp
+++ yolov5/src/calibrator.cpp
@@ -1,12 +1,13 @@
 #include "calibrator.h"
-#include "cuda_utils.h"
-#include "utils.h"
 
+#include <fstream>
 #include <iostream>
 #include <iterator>
-#include <fstream>
-#include <opencv2/opencv.hpp>
 #include <opencv2/dnn/dnn.hpp>
+#include <opencv2/opencv.hpp>
+
+#include "cuda_utils.h"
+#include "utils.h"
 
 static cv::Mat preprocess_img(cv::Mat& img, int input_w, int input_h) {
   int w, h, x, y;
@@ -30,7 +31,11 @@ static cv::Mat preprocess_img(cv::Mat& img, int input_w, int input_h) {
   return out;
 }
 
-Int8EntropyCalibrator2::Int8EntropyCalibrator2(int batchsize, int input_w, int input_h, const char* img_dir, const char* calib_table_name, const char* input_blob_name, bool read_cache)
+Int8EntropyCalibrator2::Int8EntropyCalibrator2(int batchsize, int input_w,
+                                               int input_h, const char* img_dir,
+                                               const char* calib_table_name,
+                                               const char* input_blob_name,
+                                               bool read_cache)
     : batchsize_(batchsize),
       input_w_(input_w),
       input_h_(input_h),
@@ -52,7 +57,8 @@ int Int8EntropyCalibrator2::getBatchSize() const TRT_NOEXCEPT {
   return batchsize_;
 }
 
-bool Int8EntropyCalibrator2::getBatch(void* bindings[], const char* names[], int nbBindings) TRT_NOEXCEPT {
+bool Int8EntropyCalibrator2::getBatch(void* bindings[], const char* names[],
+                                      int nbBindings) TRT_NOEXCEPT {
   if (img_idx_ + batchsize_ > (int)img_files_.size()) {
     return false;
   }
@@ -69,29 +75,35 @@ bool Int8EntropyCalibrator2::getBatch(void* bindings[], const char* names[], int
     input_imgs_.push_back(pr_img);
   }
   img_idx_ += batchsize_;
-  cv::Mat blob = cv::dnn::blobFromImages(input_imgs_, 1.0 / 255.0, cv::Size(input_w_, input_h_), cv::Scalar(0, 0, 0), true, false);
+  cv::Mat blob = cv::dnn::blobFromImages(input_imgs_, 1.0 / 255.0,
+                                         cv::Size(input_w_, input_h_),
+                                         cv::Scalar(0, 0, 0), true, false);
 
-  CUDA_CHECK(cudaMemcpy(device_input_, blob.ptr<float>(0), input_count_ * sizeof(float), cudaMemcpyHostToDevice));
+  CUDA_CHECK(cudaMemcpy(device_input_, blob.ptr<float>(0),
+                        input_count_ * sizeof(float), cudaMemcpyHostToDevice));
   assert(!strcmp(names[0], input_blob_name_));
   bindings[0] = device_input_;
   return true;
 }
 
-const void* Int8EntropyCalibrator2::readCalibrationCache(size_t& length) TRT_NOEXCEPT {
+const void* Int8EntropyCalibrator2::readCalibrationCache(size_t& length)
+    TRT_NOEXCEPT {
   std::cout << "reading calib cache: " << calib_table_name_ << std::endl;
   calib_cache_.clear();
   std::ifstream input(calib_table_name_, std::ios::binary);
   input >> std::noskipws;
   if (read_cache_ && input.good()) {
-    std::copy(std::istream_iterator<char>(input), std::istream_iterator<char>(), std::back_inserter(calib_cache_));
+    std::copy(std::istream_iterator<char>(input), std::istream_iterator<char>(),
+              std::back_inserter(calib_cache_));
   }
   length = calib_cache_.size();
   return length ? calib_cache_.data() : nullptr;
 }
 
-void Int8EntropyCalibrator2::writeCalibrationCache(const void* cache, size_t length) TRT_NOEXCEPT {
-  std::cout << "writing calib cache: " << calib_table_name_ << " size: " << length << std::endl;
+void Int8EntropyCalibrator2::writeCalibrationCache(const void* cache,
+                                                   size_t length) TRT_NOEXCEPT {
+  std::cout << "writing calib cache: " << calib_table_name_
+            << " size: " << length << std::endl;
   std::ofstream output(calib_table_name_, std::ios::binary);
   output.write(reinterpret_cast<const char*>(cache), length);
 }
-
