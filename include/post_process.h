13b6cfe5905be67e23dbfb92ac88bcc9ef1e1aea
Ervin <ervin.gan@voliro.com>
2023-02-16 14:53:39 +0100

    Corrrected linter errors for post processing files

--- include/post_process.h
+++ include/post_process.h
@@ -1,18 +1,21 @@
 #pragma once
 
-#include <opencv2/opencv.hpp>
 #include <vector>
+
+#include <opencv2/opencv.hpp>
 
 #include "../include/types.h"
 
 cv::Rect CreateRectangle(const cv::Mat& image, float bounding_box[4]);
 
-void ApplyNonMaxSuppresion(std::vector<Detection>* result, float* output,
-                           float confidence_tresh, float nms_thresh);
+void ApplyNonMaxSuppresion(
+    std::vector<Detection>* result, float* output, float confidence_tresh,
+    float nms_thresh);
 
 void ApplyBatchNonMaxSuppression(
     std::vector<std::vector<Detection>>* batch_result, float* output,
     int batch_size, int output_size, float confidence_tresh, float nms_thresh);
 
-void DrawBox(const std::vector<cv::Mat>& image_batch,
-             const std::vector<std::vector<Detection>>& result_batch);
+void DrawBox(
+    const std::vector<cv::Mat>& image_batch,
+    const std::vector<std::vector<Detection>>& result_batch);
