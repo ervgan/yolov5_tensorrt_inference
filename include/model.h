bcc13fa7c90f00f2826ec61d968dcbf630e626bf
Ervin <ervin.gan@voliro.com>
2023-03-14 15:10:36 +0100

    added all using declarations in header for model

--- include/model.h
+++ include/model.h
@@ -4,10 +4,33 @@
 
 #include <string>
 
+using nvinfer1::using nvinfer1::ActivationType;
+using nvinfer1::BuilderFlag;
 using nvinfer1::DataType;
+using nvinfer1::Dims3;
+using nvinfer1::DimsHW;
+using nvinfer1::ElementWiseOperation;
 using nvinfer1::IBuilder;
 using nvinfer1::IBuilderConfig;
+using nvinfer1::IConvolutionLayer;
 using nvinfer1::ICudaEngine;
+using nvinfer1::IPluginV2;
+using nvinfer1::IPluginV2Layer;
+using nvinfer1::PluginField;
+using nvinfer1::PluginFieldCollection;
+using nvinfer1::PluginFieldType;
+using nvinfer1::PoolingType;
+using nvinfer1::ResizeMode;
+// base interface for all layers in TensorRT
+using nvinfer1::ILayer;
+// inherits from ILayer
+using nvinfer1::IScaleLayer;
+// Neural network definition: specifies layers and connections between them
+using nvinfer1::INetworkDefinition;
+using nvinfer1::ITensor;
+// specifies how scale values are applied to input tensor of a layer
+using nvinfer1::ScaleMode;
+using nvinfer1::Weights;
 
 ICudaEngine* BuildDetectionEngine(unsigned int maxBatchSize, IBuilder* builder,
                                   IBuilderConfig* config, DataType dt,
--- src/model.cpp
+++ src/model.cpp
@@ -11,34 +11,6 @@
 #include "../include/config.h"
 #include "../include/yolo_layer.h"
 
-using nvinfer1::ActivationType;
-using nvinfer1::BuilderFlag;
-using nvinfer1::DataType;
-using nvinfer1::Dims3;
-using nvinfer1::DimsHW;
-using nvinfer1::ElementWiseOperation;
-using nvinfer1::IBuilder;
-using nvinfer1::IBuilderConfig;
-using nvinfer1::IConvolutionLayer;
-using nvinfer1::ICudaEngine;
-using nvinfer1::IPluginV2;
-using nvinfer1::IPluginV2Layer;
-using nvinfer1::PluginField;
-using nvinfer1::PluginFieldCollection;
-using nvinfer1::PluginFieldType;
-using nvinfer1::PoolingType;
-using nvinfer1::ResizeMode;
-// base interface for all layers in TensorRT
-using nvinfer1::ILayer;
-// inherits from ILayer
-using nvinfer1::IScaleLayer;
-// Neural network definition: specifies layers and connections between them
-using nvinfer1::INetworkDefinition;
-using nvinfer1::ITensor;
-// specifies how scale values are applied to input tensor of a layer
-using nvinfer1::ScaleMode;
-using nvinfer1::Weights;
-
 // C++ implementation of Yolov5 modules in models/common.py
 // https://github.com/ultralytics/yolov5/blob/d02ee60512c50d9573bb7a136d8baade8a0bd332/models/common.py#L159
 // Builds all the layers of the detection engine
