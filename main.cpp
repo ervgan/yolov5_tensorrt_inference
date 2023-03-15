3f56ac4a9789a6eb98f1c887c90e9c26f15eee73
Ervin <ervin.gan@voliro.com>
2023-03-15 14:52:24 +0100

    changed using declarations

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "../include/config.h"
 #include "../include/yolo_detector.h"
 
-using yolov5_inference::Init;
+using yolov5_inference::kGpuId;
+using yolov5_inference::States;
 using yolov5_inference::devel::DrawDetections;
 using yolov5_inference::devel::ProcessImages;
+using yolov5_inference::YoloDetector::Init;
 
 int main(int argc, char** argv) {
   yolov5_inference::YoloDetector yoloDetector;
