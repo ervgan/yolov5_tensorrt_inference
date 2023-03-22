bb40c3f7f601af1efe3fedc231e78ffd1b791b16
ervin <ervin.gan@voliro.com>
2023-03-22 11:23:18 +0100

    some changes

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CUDA"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CUDA
+  "/home/volirogpu/tensorrt_yolov5_cpp_inf/src/pre_process.cu" "/home/volirogpu/tensorrt_yolov5_cpp_inf/build/CMakeFiles/main.dir/src/pre_process.cu.o"
+  "/home/volirogpu/tensorrt_yolov5_cpp_inf/src/yolo_layer.cu" "/home/volirogpu/tensorrt_yolov5_cpp_inf/build/CMakeFiles/main.dir/src/yolo_layer.cu.o"
+  )
+set(CMAKE_CUDA_COMPILER_ID "NVIDIA")
+
+# The include file search paths:
+set(CMAKE_CUDA_TARGET_INCLUDE_PATH
+  "/home/nvidia/TensorRT-8.2.5.1/include"
+  "../include"
+  "/usr/include/opencv4"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/volirogpu/tensorrt_yolov5_cpp_inf/main.cpp" "/home/volirogpu/tensorrt_yolov5_cpp_inf/build/CMakeFiles/main.dir/main.cpp.o"
+  "/home/volirogpu/tensorrt_yolov5_cpp_inf/src/model.cpp" "/home/volirogpu/tensorrt_yolov5_cpp_inf/build/CMakeFiles/main.dir/src/model.cpp.o"
+  "/home/volirogpu/tensorrt_yolov5_cpp_inf/src/post_process.cpp" "/home/volirogpu/tensorrt_yolov5_cpp_inf/build/CMakeFiles/main.dir/src/post_process.cpp.o"
+  "/home/volirogpu/tensorrt_yolov5_cpp_inf/src/yolo_detector.cpp" "/home/volirogpu/tensorrt_yolov5_cpp_inf/build/CMakeFiles/main.dir/src/yolo_detector.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  "/home/nvidia/TensorRT-8.2.5.1/include"
+  "../include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/src/model.cpp.o"
+  "CMakeFiles/main.dir/src/post_process.cpp.o"
+  "CMakeFiles/main.dir/src/pre_process.cu.o"
+  "CMakeFiles/main.dir/src/yolo_detector.cpp.o"
+  "CMakeFiles/main.dir/src/yolo_layer.cu.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
